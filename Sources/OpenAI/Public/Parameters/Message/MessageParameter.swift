e8d607a1865ddfff5a7d9002f9c17d47bb892454
mludowise <mludowise@gmail.com>
2025-02-25 14:09:19 -0800

    fix url

--- Sources/OpenAI/Public/Parameters/Message/MessageParameter.swift
+++ Sources/OpenAI/Public/Parameters/Message/MessageParameter.swift
@@ -81,7 +81,7 @@ public struct MessageParameter: Encodable {
    public struct ImageURL: Encodable {
       
       /// The external URL of the image, must be a supported image types: jpeg, jpg, png, gif, webp.
-      let url: URL
+      let url: String
 
       /// Specifies the detail level of the image. low uses fewer tokens, you can opt in to high resolution using high. Default value is auto
       let detail: DetailLevel?
